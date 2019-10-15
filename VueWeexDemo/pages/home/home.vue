dc868e9ba3eccdd4021539977189bf3768494439
门超 <menchao@yeah.net>
2019-10-15 17:26:17 +0800

    首页下拉刷新

---
+++ VueWeexDemo/pages/home/home.vue
@@ -0,0 +1,34 @@
+<template>
+	<view style="background-color:#999999">
+	asgd 
+	</view>
+</template>
+
+<script>
+	export default {
+		 data() {return {num: 1}},
+		        created() {console.log('页面created')},
+		        methods: {
+		            changeNum() { this.num += 1; },
+		            showModal(type) {
+		                if (type === 'uni') {
+		                    uni.showModal({content: '使用 uni-app 的 API 弹出模态框'});
+		                }
+		                else if (type === 'plus') {
+		                    plus.nativeUI.alert('使用 uni-app 的 API 弹出模态框');
+		                } else {
+		                    var modal = weex.requireModule('modal')
+		                    modal.alert({message: '使用 weex 的 API 弹出模态框'});
+		                }
+		            }
+		        }
+	}
+</script>
+
+<style>
+	  .example {flex-direction: column;}
+	    .text {line-height: 80px;font-size: 32px;color: #333;}
+	    .item {height: 80px;width: 690px;margin: 30px;background-color: #f8f8f8;border-width: 1px;border-color: #eee;border-radius: 10px;align-items: center;}
+
+
+</style>
---
+++ VueWeexDemo/test/test.js
@@ -0,0 +1,22 @@
+var info = {
+    name: 'zs',
+    age: 20
+}
+
+// const getSum = (a,b) => {
+//     return a+b;
+// }
+
+var getSum = function (a,b){
+	return a+b;
+}
+
+
+
+export default {info}
+
+ var title  = '小星星'
+ 
+ var content = '哈哈哈'
+
+export {getSum,title,content}
